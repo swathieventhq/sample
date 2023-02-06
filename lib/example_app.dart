4862f5dd036c8fa01a689475eacd9ffd3b0de00d
swathieventhq <swathi.chandraiah@eventhq.io>
2023-02-06 16:15:11 +0530

    Initial commit

---
+++ lib/example_app.dart
@@ -0,0 +1,87 @@
+import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'constants.dart';
+import 'hero.dart';
+import 'user.dart';
+
+class ExampleApp extends StatefulWidget {
+  final Auth0? auth0;
+  const ExampleApp({this.auth0, final Key? key}) : super(key: key);
+
+  @override
+  State<ExampleApp> createState() => _ExampleAppState();
+}
+
+class _ExampleAppState extends State<ExampleApp> {
+  UserProfile? _user;
+
+  late Auth0 auth0;
+
+  @override
+  void initState() {
+    super.initState();
+    auth0 = widget.auth0 ??
+        Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+  }
+
+  Future<void> login() async {
+    var credentials = await auth0.webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
+        .login();
+
+    setState(() {
+      _user = credentials.user;
+    });
+  }
+
+  Future<void> logout() async {
+    await auth0
+        .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
+        .logout();
+
+    setState(() {
+      _user = null;
+    });
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.only(
+          top: padding,
+          bottom: padding,
+          left: padding / 2,
+          right: padding / 2,
+        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Expanded(
+              child: Column(children: [
+            _user != null
+                ? UserWidget(user: _user)
+                : const Expanded(child: HeroWidget())
+          ])),
+          _user != null
+              ? ElevatedButton(
+                  onPressed: logout,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  child: const Text('Logout'),
+                )
+              : ElevatedButton(
+                  onPressed: login,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  child: const Text('Login'),
+                )
+        ]),
+      )),
+    );
+  }
+}
